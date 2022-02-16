47d173b57d1802a452075bac2b959e0cf2f748ba
Karen <karenvane129@gmail.com>
2022-02-16 14:03:26 -0500

    cambios welcome

--- frontreact/src/pages/Welcome.js
+++ frontreact/src/pages/Welcome.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Welcome() {
   return (
-    <div>
       
       <div className="btn">
-      <Link className="btn btn-success btn-lg" role="button" to="Landing">
-        Find your space
-      </Link>
-
-      <button>Publish your space</button>
+        <Link className="btn btn-success btn-lg" role="button" to="Landing">
+          Find your space
+        </Link>
+        <Link className="btn btn-success btn-lg" role="button" to="Landing">
+          Publish your space
+        </Link>
     </div>
   );
 }
