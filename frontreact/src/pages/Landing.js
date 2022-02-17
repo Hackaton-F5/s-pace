1e993791b9bb5cd1200da27e3ce5fbb40c3f7064
MayuRuru <aruizrull@protonmail.com>
2022-02-17 09:26:16 +0100

    push to dev

--- frontreact/src/pages/Landing.js
+++ frontreact/src/pages/Landing.js
@@ -2,13 +2,14 @@ import CardGrid from "../components/CardGrid";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import React from "react";
+import CardGridCopy from "../components/CardGridCopy";
 
 function Landing() {
   return (
     <div>
       <h1>Landing</h1>
       <Navbar />
-      <CardGrid />
+      <CardGridCopy />
       <Footer />
     </div>
   );
